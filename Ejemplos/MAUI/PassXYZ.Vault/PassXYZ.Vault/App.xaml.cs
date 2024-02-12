5621d81e73a6b3e9dd938ccdfde1999d17aae548
Daniel Ojeda Ubeda <43439678+Djeda8@users.noreply.github.com>
2024-02-12 19:29:44 +0000

    initial push

---
+++ Ejemplos/MAUI/PassXYZ.Vault/PassXYZ.Vault/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using PassXYZ.Vault.Services;
+
+namespace PassXYZ.Vault
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            return new PxWindow(MainPage);
+        }
+    }
+}
+
---
+++ Ejemplos/MAUI/PassXYZ.Vault/PassXYZ.Vault/ViewModels/AboutViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
+
+namespace PassXYZ.Vault.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = Properties.Resources.About;
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync(Properties.Resources.about_url));
+        }
+
+        public ICommand OpenWebCommand { get; }
+
+        public string GetStoreName()
+        {
+            return "Test Database";
+        }
+
+        public DateTime GetStoreModifiedTime()
+        {
+            return DateTime.Now;
+        }
+
+    }
+}
